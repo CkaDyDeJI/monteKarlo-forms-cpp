1c4f3d810bb2bac6b570d56541e48f98a085ef64
CkaDyDeJI <55078827+CkaDyDeJI@users.noreply.github.com>
2020-06-06 16:13:42 +0300

    after redesign

--- monteKarlo-forms-cpp/MyForm.h
+++ monteKarlo-forms-cpp/MyForm.h
@@ -52,15 +52,16 @@ namespace monteKarloformscpp
 		System::Windows::Forms::TextBox^ textBox3;
 		System::Windows::Forms::Button^ button1;
 		System::Windows::Forms::DataGridView^ dataGridView1;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ nPoints;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ nPointsInside;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ square;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ mkSquare;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ accuracy;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ time;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::Label^ label4;
+		System::Windows::Forms::DataGridViewTextBoxColumn^ nPoints;
+		System::Windows::Forms::DataGridViewTextBoxColumn^ nPointsInside;
+		System::Windows::Forms::DataGridViewTextBoxColumn^ square;
+		System::Windows::Forms::DataGridViewTextBoxColumn^ mkSquare;
+		System::Windows::Forms::DataGridViewTextBoxColumn^ accuracy;
+		System::Windows::Forms::DataGridViewTextBoxColumn^ time;
+		System::Windows::Forms::PictureBox^ pictureBox1;
+		System::Windows::Forms::TextBox^ textBox4;
+		System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
 
 
 
@@ -97,82 +98,90 @@ namespace monteKarloformscpp
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox ());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox ());
 			this->label4 = (gcnew System::Windows::Forms::Label ());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox ());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit ();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit ();
+			this->groupBox1->SuspendLayout ();
 			this->SuspendLayout ();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point (39, 291);
+			this->label1->Location = System::Drawing::Point (6, 30);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size (83, 13);
+			this->label1->Size = System::Drawing::Size (45, 13);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"левая точка (b)";
+			this->label1->Text = L"точка A";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point (39, 338);
+			this->label2->Location = System::Drawing::Point (6, 65);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size (94, 13);
+			this->label2->Size = System::Drawing::Size (45, 13);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"верхняя точка (c)";
+			this->label2->Text = L"точка B";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point (39, 385);
+			this->label3->Location = System::Drawing::Point (6, 103);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size (89, 13);
+			this->label3->Size = System::Drawing::Size (45, 13);
 			this->label3->TabIndex = 2;
-			this->label3->Text = L"правая точка (d)";
+			this->label3->Text = L"точка C";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point (134, 288);
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox1->Location = System::Drawing::Point (122, 27);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size (100, 20);
 			this->textBox1->TabIndex = 3;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point (134, 335);
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox2->Location = System::Drawing::Point (122, 62);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size (100, 20);
 			this->textBox2->TabIndex = 4;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point (134, 382);
+			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox3->Location = System::Drawing::Point (122, 100);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size (100, 20);
 			this->textBox3->TabIndex = 5;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point (263, 307);
+			this->button1->BackColor = System::Drawing::SystemColors::Control;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Location = System::Drawing::Point (281, 77);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size (75, 23);
 			this->button1->TabIndex = 6;
 			this->button1->Text = L"вычислить";
-			this->button1->UseVisualStyleBackColor = true;
+			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler (this, &MyForm::button1_Click);
 			// 
 			// dataGridView1
 			// 
-			this->dataGridView1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+			this->dataGridView1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::Control;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dataGridView1->Columns->AddRange (gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  > (6)
 			{
 				this->nPoints,
 					this->nPointsInside, this->square, this->mkSquare, this->accuracy, this->time
 			});
-			this->dataGridView1->Location = System::Drawing::Point (0, 1);
+			this->dataGridView1->Location = System::Drawing::Point (0, 194);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size (1085, 264);
+			this->dataGridView1->Size = System::Drawing::Size (687, 201);
 			this->dataGridView1->TabIndex = 7;
 			// 
 			// nPoints
@@ -209,7 +218,7 @@ namespace monteKarloformscpp
 			// 
 			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject (L"pictureBox1.BackgroundImage")));
 			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
-			this->pictureBox1->Location = System::Drawing::Point (761, 271);
+			this->pictureBox1->Location = System::Drawing::Point (375, 19);
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->Size = System::Drawing::Size (312, 169);
 			this->pictureBox1->TabIndex = 8;
@@ -217,7 +226,8 @@ namespace monteKarloformscpp
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point (134, 420);
+			this->textBox4->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox4->Location = System::Drawing::Point (122, 139);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size (100, 20);
 			this->textBox4->TabIndex = 9;
@@ -225,34 +235,45 @@ namespace monteKarloformscpp
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point (39, 426);
+			this->label4->Location = System::Drawing::Point (6, 142);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size (35, 13);
+			this->label4->Size = System::Drawing::Size (46, 13);
 			this->label4->TabIndex = 10;
-			this->label4->Text = L"label4";
+			this->label4->Text = L"точка D";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add (this->label4);
+			this->groupBox1->Controls->Add (this->label1);
+			this->groupBox1->Controls->Add (this->textBox4);
+			this->groupBox1->Controls->Add (this->label2);
+			this->groupBox1->Controls->Add (this->label3);
+			this->groupBox1->Controls->Add (this->textBox1);
+			this->groupBox1->Controls->Add (this->textBox2);
+			this->groupBox1->Controls->Add (this->textBox3);
+			this->groupBox1->Location = System::Drawing::Point (0, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size (257, 176);
+			this->groupBox1->TabIndex = 11;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"точки";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF (6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size (1085, 649);
-			this->Controls->Add (this->label4);
-			this->Controls->Add (this->textBox4);
+			this->ClientSize = System::Drawing::Size (686, 395);
+			this->Controls->Add (this->button1);
+			this->Controls->Add (this->groupBox1);
 			this->Controls->Add (this->pictureBox1);
 			this->Controls->Add (this->dataGridView1);
-			this->Controls->Add (this->button1);
-			this->Controls->Add (this->textBox3);
-			this->Controls->Add (this->textBox2);
-			this->Controls->Add (this->textBox1);
-			this->Controls->Add (this->label3);
-			this->Controls->Add (this->label2);
-			this->Controls->Add (this->label1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit ();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit ();
+			this->groupBox1->ResumeLayout (false);
+			this->groupBox1->PerformLayout ();
 			this->ResumeLayout (false);
-			this->PerformLayout ();
 
 		}
 
@@ -261,7 +282,10 @@ namespace monteKarloformscpp
 
 		
 #pragma endregion
-		System::Void button1_Click (System::Object^ sender, System::EventArgs^ e)
+
+
+		
+		Void button1_Click (System::Object^ sender, System::EventArgs^ e)
 		{	//при нажатии на кнопку начало вычисления площади
 			if (setPoints () == false) {	//проверка того, ввелись ли пользователем точки
 				return;
